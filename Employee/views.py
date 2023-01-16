2f81f1955cb8bbb756a02c0d56543f1191835ea1
mihirebiz <mihir.chaudhari@esparkbizmail.com>
2023-01-16 19:56:03 +0530

    master

--- Employee/views.py
+++ Employee/views.py
@@ -5,8 +5,30 @@ function HomeTab() {
   const [visible, setVisible] = useState(false);
 
   return (
-    
-          <Tab visible={visible} className="className" title="Why do it">
+    <div className="tabs-main">
+      <FadeInOut className="slower">
+        <Tabs setVisible={setVisible} visible={visible}>
+          <Tab visible={v  <Tab visible={visible} className="className" title="Why do it">
+            <p className="mb-4">Why do it</p>
+            <p className="mb-4">
+              {" "}
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>
+          </Tab>
+          <Tab visible={visible} className="className" title="How it works">
+            <p className="mb-4">How it works</p>
+            <p className="mb-4">
+              {" "}
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>
+          </Tab>i  <Tab visible={visible} className="className" title="Why do it">
+            <p className="mb-4">Why do it</p>
+            <p className="mb-4">
+              {" "}  <Tab visible={visible} className="className" title="Why do it">
             <p className="mb-4">Why do it</p>
             <p className="mb-4">
               {" "}
@@ -24,6 +46,83 @@ function HomeTab() {
               on the map to get started.
             </p>
           </Tab>
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>  <Tab visible={visible} className="className" title="Why do it">
+            <p className="mb-4">Why do it</p>
+            <p className="mb-4">
+              {" "}
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to g  <Tab visible={visible} className="className" title="Why do it">
+            <p className="mb-4">Why do it</p>
+            <p className="mb-4">
+              {" "}
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>
+          </Tab>
+          <Tab visible={visible} className="className" title="How it works">
+            <p className="mb-4">How it works</p>
+            <p className="mb-4">
+              {" "}
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>
+          </Tab>et started.
+            </p>
+          </Tab>
+          <Tab visible={visible} className="className" title="How it works">
+            <p className="mb-4">How it works</p>
+            <p className="mb-4">
+              {" "}
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>
+          </Tab>
+          </Tab>
+          <Tab visible={visible} className="className" title="How it works">
+            <p className="mb-4">How it works</p>
+            <p className="mb-4">
+              {" "}
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>
+          </Tab>sible} className="className" title="What is it">
+            <p className="mb-4">
+              It’s a fi  <Tab visible={visible} className="className" title="Why do it">
+            <p className="mb-4">Why do it</p>
+            <p className="mb-4">
+              {" "}
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>
+          </Tab>
+          <Tab visible={visible} className="className" title="How it works">
+            <p className="mb-4">How it works</p>
+            <p className="mb-4">
+              {" "}
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>
+          </Tab>rst-in, first-served opportunity for you to own any map
+              locations or landmarks anywhere in the world - FOREVER!
+            </p>
+            <p className="mb-4">
+              Have fun connecting & sharing, or promote yourself or business
+              through your very own box-in-the-world. Hit the Get a box button
+              on the map to get started.
+            </p>
+          </Tab>
+    
+        
         </Tabs>
       </FadeInOut>
     </div>
