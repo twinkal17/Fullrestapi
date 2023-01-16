63ba1e9fba8b168440ae7d10c5bb0fa641d966ad
mihirebiz <mihir.chaudhari@esparkbizmail.com>
2023-01-16 19:47:28 +0530

    master--> mihirebiz

--- Employee/models.py
+++ Employee/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 
+
 class Employee(models.Model):
     fullname12 = models.CharField(max_length=500)
-    emp_code = models.CharField(max_length=300)
     mobile = models.CharField(max_length=300)
+    emp_code = models.CharField(max_length=300)
+
 
 class Example(models.Model):
-    fullname123 = models.CharField(max_length=500)
     emp_code = models.CharField(max_length=300)
-
-
-#commiit
-# Commment
+    fullname123 = models.CharField(max_length=500)
